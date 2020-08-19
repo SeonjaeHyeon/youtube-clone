5db53ef2b779792aaa3a7846ebba26f544d7c21b
SeonjaeHyeon <hsj106@mju.ac.kr>
2020-08-20 03:05:56 +0900

    feat: Add database for video

--- server.py
+++ server.py
@@ -1,24 +1,22 @@
 from flask import Flask
-import cv2, os
+from flask import render_template
+import cv2, os, json
 
 app = Flask(__name__)
 
+with open('db.json', 'r') as f:
+    db = json.load(f)
+
 @app.route('/')
 def home():
-    file_list = os.listdir('./static/videos/')
-    video_list = [file for file in file_list if file.endswith(".mp4")] # https://itholic.github.io/python-listdir-glob/
-
-    for f in video_list:
-        if not os.path.isfile('./static/thumbnails/' + f[:-4] + '.jpg'):
-            cap = cv2.VideoCapture('./static/videos/' + f)
+    for name, video in db.items():
+        if not os.path.isfile('./static/' + video['thumnail']):
+            cap = cv2.VideoCapture('./static/' + video['path'])
             ret, img = cap.read()
-            # why use os.getcwd(): https://daeunginfo.blogspot.com/2019/07/opencv-python-error-215assertion-failed.html?m=1
-            cv2.imwrite(os.path.join(os.getcwd(), 'static', 'thumnails', '%s.jpg' % f[:-4]), img)
+            cv2.imwrite(os.path.join(os.getcwd(), 'static', 'thumnails', '%s.jpg' % name), img)
             cap.release()
 
-    return render_template('index.html')
-
-from flask import render_template
+    return render_template('index.html', db=db)
 
 @app.route('/watch/')
 @app.route('/watch/<name>')
