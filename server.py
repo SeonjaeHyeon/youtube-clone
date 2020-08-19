4fd003cf3788e4d4793cf7acecfa52b70ae31e05
SeonjaeHyeon <hsj106@mju.ac.kr>
2020-08-19 22:01:46 +0900

    feat: Add homepage & sample video page

--- server.py
+++ server.py
@@ -2,5 +2,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return render_template('home.html')
+
+from flask import render_template
+
+@app.route('/watch/')
+@app.route('/watch/<name>')
+def watch(name=None):
+    return render_template('watch.html', name=name)
